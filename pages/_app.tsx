38bf031b32072a93f4e38eb919ad7b8a63f8d421
cottons-kr <devkim@hanarin.uk>
2024-05-31 04:24:39 +0000

    🔍 Add metadata to improve SEO and social media sharing

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,9 +4,25 @@ import { appWithTranslation } from 'next-i18next'
 import '@/styles/globals.scss'
 import '@fortawesome/fontawesome-svg-core/styles.css'
 import { Footer } from '@/components/Footer'
+import { Head } from 'next/document'
 
 function App({ Component, pageProps }: AppProps) {
   return <>
+    <Head>
+      <title>Taeyeong Kim</title>
+      <meta name='description' content={`Taeyeong Kim's Portfolio`} />
+      <meta name='viewport' content='width=device-width, initial-scale=1' />
+      <meta name='theme-color' content='#9BE8D8' />
+      <meta name='author' content='Taeyeong Kim' />
+      <meta name='keywords' content='Taeyeong Kim, Portfolio, Software Engineer, Web Developer, Full Stack Developer' />
+      <meta name='robots' content='index, follow' />
+      <meta name='googlebot' content='index, follow' />
+      <meta property='og:title' content='Taeyeong Kim' />
+      <meta property='og:description' content={`Taeyeong Kim's Portfolio`} />
+      <meta property='og:type' content='website' />
+      <meta property='og:url' content='https://tyeongkim.me/' />
+      <meta property='og:site_name' content='Taeyeong Kim' />
+    </Head>
     <main>
       <Component {...pageProps} />
       <Footer />
