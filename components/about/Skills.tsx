56729bbc374d29ed842d745599d9ecee26e401fa
cottons-kr <devkim@hanarin.uk>
2024-06-12 16:59:42 +0000

    🎨 Replace var to const

--- components/about/Skills.tsx
+++ components/about/Skills.tsx
@@ -435,10 +435,10 @@ function SkillTooltip(props: Skill) {
   const { name, subfix, description } = props
   const getContrastYIQ = (hexcolor: string) => {
     hexcolor = hexcolor.replace('#', '')
-    var r = parseInt(hexcolor.substring(0, 2), 16)
-    var g = parseInt(hexcolor.substring(2, 4), 16)
-    var b = parseInt(hexcolor.substring(4, 6), 16)
-    var yiq = ((r * 299) + (g * 587) + (b * 114)) / 1000
+    const r = parseInt(hexcolor.substring(0, 2), 16)
+    const g = parseInt(hexcolor.substring(2, 4), 16)
+    const b = parseInt(hexcolor.substring(4, 6), 16)
+    const yiq = ((r * 299) + (g * 587) + (b * 114)) / 1000
     return (yiq >= 128) ? 'var(--font-color)' : 'white'
   }
 
